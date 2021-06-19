965d1a8a161efcdfde363c39d5eefe5a80a1934b
ssali001 <shabo.saliba@student.hs-rm.de>
2021-06-19 21:47:41 +0200

    Connect-the-API-for-Produkt-hinzufuegen-in-Frontend hat erfolgreich geklappt

--- webshop/src/components/addProduct/AddProduct.vue
+++ webshop/src/components/addProduct/AddProduct.vue
@@ -1,16 +1,86 @@
 <template>
   <div>
+    <router-link to="/admin" class="deco-none">
+      <button>zurück</button>
+    </router-link>
     <h1>Artikel hinzufügen</h1>
-    <form action="">
-      <label for="name">Artikel Name:</label><br />
-      <input type="text" id="name" name="name" /><br /><br />
-      <label for="price">Artikel Preis:</label><br />
-      <input type="text" id="price" name="price" /><br /><br />
-      <label for="description">Artikel Beschreibung:</label><br />
-      <input type="text" id="description" name="description" /><br /><br />
-      <label for="image">Artikel Bild hochladen:</label><br />
-      <input type="file" id="image" name="image" /><br /><br />
+    <form id="addProduct-form" v-on:submit="addProduct">
+      <label for="productName">Artikel Name:</label><br />
+      <input
+        type="text"
+        id="productName"
+        name="productName"
+        v-model="form.productName"
+        required
+      /><br /><br />
+      <label for="productPrice">Artikel Preis:</label><br />
+      <input
+        type="text"
+        id="productPrice"
+        name="productPrice"
+        v-model="form.productPrice"
+        required
+      /><br /><br />
+      <label for="productDescription">Artikel Beschreibung:</label><br />
+      <input
+        type="text"
+        id="productDescription"
+        name="productDescription"
+        v-model="form.productDescription"
+        required
+      /><br /><br />
+      <label for="productImage">Artikel Bild hochladen:</label><br />
+      <input
+        type="file"
+        id="productImage"
+        name="productImage"
+        required
+      /><br /><br />
       <input type="submit" value="hinzufügen" />
     </form>
   </div>
 </template>
+<script>
+export default {
+  name: "AddProduct",
+  data() {
+    return {
+      form: {
+        productName: "",
+        productPrice: "",
+        productDescription: "",
+      },
+    };
+  },
+  methods: {
+    addProduct(e) {
+      e.preventDefault(); // it prevent from page reload and prevent inputfield removal
+      const cloudfunctions_produkts_URL =
+        "https://europe-west3-webshop-316612.cloudfunctions.net/produkte/";
+
+      var imageName = document.getElementById("productImage").value;
+
+      fetch(cloudfunctions_produkts_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          produktName: this.form.productName,
+          produktPreis: this.form.productPrice + " $",
+          produktBeschreibung: this.form.productDescription,
+          produktImage: imageName.split(/(\\|\/)/g).pop(),
+        }),
+      })
+        // .then((data) => console.log(data))
+        .catch((error) => console.log("Error", error));
+
+      // Empty The Input Fields after Submit
+      document.getElementById("productName").value = "";
+      document.getElementById("productPrice").value = "";
+      document.getElementById("productDescription").value = "";
+      document.getElementById("productImage").value = "";
+    },
+  },
+};
+</script>
--- webshop/src/components/showAllUsers/ShowAllUsers.vue
+++ webshop/src/components/showAllUsers/ShowAllUsers.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Alle Users anzeigen</h1>
+  <div>
+    <router-link to="/admin" class="deco-none">
+      <button>zurück</button>
+    </router-link>
+    <h1>Alle Users anzeigen</h1>
+  </div>
 </template>
