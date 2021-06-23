d0c129176fb918c6e2111d84ec49beeac61dc759
ssali001 <shabo.saliba@student.hs-rm.de>
2021-06-23 23:53:01 +0200

    Styling-Admin-Page was Successful git add .!

--- webshop/src/components/admin/Admin.vue
+++ webshop/src/components/admin/Admin.vue
@@ -1,24 +1,45 @@
 <template>
-  <div>
+  <div class="container-center container-admin">
     <h1>Willkommen Admin</h1>
-    <table>
-      <tr>
-        <td>
-          <router-link to="/addProduct" class="deco-none">
-            <button>Artikel hinzufügen</button>
-          </router-link>
-        </td>
-        <td>
-          <router-link to="/showAllProducts" class="deco-none">
-            <button>Alle Artikeln anzeigen</button>
-          </router-link>
-        </td>
-        <td>
-          <router-link to="/showAllUsers" class="deco-none">
-            <button>Alle Users anzeigen</button>
-          </router-link>
-        </td>
-      </tr>
-    </table>
+    <router-link to="/addProduct" class="deco-none">
+      <button class="btn btn-primary admin-buttons">
+        Artikel hinzufügen
+      </button>
+    </router-link>
+
+    <router-link to="/showAllProducts" class="deco-none">
+      <button class="btn btn-primary admin-buttons">
+        Alle Artikeln anzeigen
+      </button>
+    </router-link>
+
+    <router-link to="/showAllUsers" class="deco-none">
+      <button class="btn btn-primary admin-buttons">
+        Alle Users anzeigen
+      </button>
+    </router-link>
   </div>
 </template>
+
+<style>
+.container-center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  border-style: outset;
+  border-radius: 25px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: 2%;
+}
+#productImage {
+  width: 70%;
+}
+label {
+  padding-bottom: 1%;
+}
+.admin-buttons {
+  margin: 1%;
+}
+</style>
