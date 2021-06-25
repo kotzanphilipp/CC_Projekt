fec0ea630b0517e02ad13e037dacf8eb642e409e
Philipp <philipp.kotzan@student.hs-rm.de>
2021-06-25 17:30:36 +0200

    added vgmaps to pkg + User.vue

--- webshop/src/components/user/User.vue
+++ webshop/src/components/user/User.vue
@@ -1,3 +1,66 @@
 <template>
-    <h1>User</h1>
+  <div class="users">
+    <h1>Users</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>vorname</th>
+          <th>nachname</th>
+          <th>telefonnummer</th>
+        </tr>
+      </thead>
+      <tbody v-for="(nutzer, index) in nutzern" v-bind:key="nutzer.email">
+        <tr>
+          <td>{{ nutzer.vorname }}</td>
+          <td>{{ nutzer.nachname }}</td>
+          <td>{{ nutzer.telefonnummer }}</td>
+          <td @click="deleteNutzer(nutzer, index)" class="btn btn-danger">
+            Löschen
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+import { onBeforeMount, reactive } from "vue";
+
+export default {
+  name: "User",
+  setup() {
+    let nutzern = reactive([]);
+    onBeforeMount(() => {
+      fetchUsers();
+    });
+    async function fetchUsers() {
+      await axios
+        .get("https://europe-west3-webshop-316612.cloudfunctions.net/users")
+        .then(function(response) {
+          nutzern.push(...response.data);
+          console.log(nutzern);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+    async function deleteNutzer(nutzer, index) {
+      console.log("nutzer_Email: " + nutzer.email);
+      axios
+        .delete(
+          "https://europe-west3-webshop-316612.cloudfunctions.net/nutzerinfo/delete/nutzer=" +
+            nutzer.email
+        )
+        .then(function() {
+          nutzern.splice(index, 1);
+          console.log;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+    return { nutzern, deleteNutzer };
+  }
+};
+</script>
