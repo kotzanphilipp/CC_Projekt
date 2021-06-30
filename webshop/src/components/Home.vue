71f1815f44e4437c93cd648a3188500215231db3
ssali001 <shabo.saliba@student.hs-rm.de>
2021-06-30 20:45:16 +0200

    Home -> Willkommen anstatt Your Role is:

--- webshop/src/components/Home.vue
+++ webshop/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="Home">
+  <div class="Home">
     <h1>Home</h1>
-    <h2>YOUR ROLE IS: {{ templ.r }}</h2>
-    </div>
+    <h2>Willkommen {{ templ.r }}</h2>
+  </div>
 </template>
 
 <script>
@@ -10,14 +10,14 @@ import useSession from "@/service/SessionStore";
 import { reactive } from "vue";
 
 export default {
-    name: "Home",
-    setup() {
-        const { role } = useSession();
+  name: "Home",
+  setup() {
+    const { role } = useSession();
 
-        const templ = reactive({
-            r: role.value,
-        })
-        return { templ };
-    },
-}
+    const templ = reactive({
+      r: role.value,
+    });
+    return { templ };
+  },
+};
 </script>
