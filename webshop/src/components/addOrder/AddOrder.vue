cfd96e1372be110a5dc5c08ee9ae70b920eff900
ssali001 <shabo.saliba@student.hs-rm.de>
2021-06-30 19:41:56 +0200

    Bestellung Abgeschlossen (Add Order Seite bisschen gestylt)

--- webshop/src/components/addOrder/AddOrder.vue
+++ webshop/src/components/addOrder/AddOrder.vue
@@ -3,19 +3,26 @@
     <router-link to="/" class="deco-none">
       <button>zurück</button>
     </router-link>
-    <h1>Bestellung abgeschlossen</h1>
+    <div class="container-center">
+      <h1 class="add-order" style="color:green">Bestellung abgeschlossen</h1>
+      <h2 class="add-order" style="color:blue">
+        Vielen Dank für Ihren Einkauf
+      </h2>
+      <h3 class="add-order" style="color:blue">
+        Ihre Bestellung ist an uns gesendet
+      </h3>
+    </div>
   </div>
 </template>
 <script>
 import { onBeforeMount, reactive } from "vue";
-
 
 export default {
   name: "AddOrder",
   setup() {
     let cart = reactive([]);
     onBeforeMount(() => {
-      readLocalStorageCart()
+      readLocalStorageCart();
       console.log(cart);
       /*fetchUsers();*/
       emptyCart();
@@ -27,12 +34,18 @@ export default {
       cart.push(...JSON.parse(localStorage["cart"]));
     }
 
-   async function emptyCart(){
-      localStorage.removeItem('cart');
+    async function emptyCart() {
+      localStorage.removeItem("cart");
     }
-  return {
-    readLocalStorageCart,
-  };
+    return {
+      readLocalStorageCart,
+    };
   },
 };
 </script>
+<style>
+.add-order {
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+</style>
